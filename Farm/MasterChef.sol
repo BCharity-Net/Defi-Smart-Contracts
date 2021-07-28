13e1bd0a997375312dd3efe041ab6aae9d589e11
zlnnnnn <55903026+zlnnnnn@users.noreply.github.com>
2021-07-28 16:38:13 -0600

    Update reference to fish token to give token

--- Farm/MasterChef.sol
+++ Farm/MasterChef.sol
@@ -7,13 +7,13 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v3.1
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v3.1.0/contracts/token/ERC20/SafeERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v3.1.0/contracts/utils/ReentrancyGuard.sol";
 
-import "./FishToken.sol";
-import "./libs/IReferral.sol";
+import "./GiveToken.sol";
+import "./IReferral.sol";
 
-// MasterChef is the master of Fish. He can make Fish and he is a fair guy.
+// MasterChef is the master of Give. He can make Give and he is a fair guy.
 //
 // Note that it's ownable and the owner wields tremendous power. The ownership
-// will be transferred to a governance smart contract once Fish is sufficiently
+// will be transferred to a governance smart contract once Give is sufficiently
 // distributed and the community can show to govern itself.
 //
 // Have fun reading it. Hopefully it's bug-free. God bless.
@@ -26,13 +26,13 @@ contract MasterChef is Ownable, ReentrancyGuard {
         uint256 amount;         // How many LP tokens the user has provided.
         uint256 rewardDebt;     // Reward debt. See explanation below.
         //
-        // We do some fancy math here. Basically, any point in time, the amount of FISHes
+        // We do some fancy math here. Basically, any point in time, the amount of GIVEes
         // entitled to a user but is pending to be distributed is:
         //
-        //   pending reward = (user.amount * pool.accFishPerShare) - user.rewardDebt
+        //   pending reward = (user.amount * pool.accGivePerShare) - user.rewardDebt
         //
         // Whenever a user deposits or withdraws LP tokens to a pool. Here's what happens:
-        //   1. The pool's `accFishPerShare` (and `lastRewardBlock`) gets updated.
+        //   1. The pool's `accGivePerShare` (and `lastRewardBlock`) gets updated.
         //   2. User receives the pending reward sent to his/her address.
         //   3. User's `amount` gets updated.
         //   4. User's `rewardDebt` gets updated.
@@ -41,20 +41,20 @@ contract MasterChef is Ownable, ReentrancyGuard {
     // Info of each pool.
     struct PoolInfo {
         IERC20 lpToken;           // Address of LP token contract.
-        uint256 allocPoint;       // How many allocation points assigned to this pool. FISHes to distribute per block.
-        uint256 lastRewardBlock;  // Last block number that FISHes distribution occurs.
-        uint256 accFishPerShare;   // Accumulated FISHes per share, times 1e18. See below.
+        uint256 allocPoint;       // How many allocation points assigned to this pool. GIVEes to distribute per block.
+        uint256 lastRewardBlock;  // Last block number that GIVEes distribution occurs.
+        uint256 accGivePerShare;   // Accumulated GIVEes per share, times 1e18. See below.
         uint16 depositFeeBP;      // Deposit fee in basis points
     }
 
-    // The FISH TOKEN!
-    FishToken public fish;
+    // The GIVE TOKEN!
+    GiveToken public give;
     address public devAddress;
     address public feeAddress;
     address public vaultAddress;
 
-    // FISH tokens created per block.
-    uint256 public fishPerBlock = 1 ether;
+    // GIVE tokens created per block.
+    uint256 public givePerBlock = 1 ether;
 
     // Info of each pool.
     PoolInfo[] public poolInfo;
@@ -62,10 +62,10 @@ contract MasterChef is Ownable, ReentrancyGuard {
     mapping(uint256 => mapping(address => UserInfo)) public userInfo;
     // Total allocation points. Must be the sum of all allocation points in all pools.
     uint256 public totalAllocPoint = 0;
-    // The block number when FISH mining starts.
+    // The block number when GIVE mining starts.
     uint256 public startBlock;
 
-    // Fish referral contract address.
+    // Give referral contract address.
     IReferral public referral;
     // Referral commission rate in basis points.
     uint16 public referralCommissionRate = 200;
@@ -79,17 +79,17 @@ contract MasterChef is Ownable, ReentrancyGuard {
     event SetDevAddress(address indexed user, address indexed newAddress);
     event SetVaultAddress(address indexed user, address indexed newAddress);
     event SetReferralAddress(address indexed user, IReferral indexed newAddress);
-    event UpdateEmissionRate(address indexed user, uint256 fishPerBlock);
+    event UpdateEmissionRate(address indexed user, uint256 givePerBlock);
     event ReferralCommissionPaid(address indexed user, address indexed referrer, uint256 commissionAmount);
 
     constructor(
-        FishToken _fish,
+        GiveToken _give,
         uint256 _startBlock,
         address _devAddress,
         address _feeAddress,
         address _vaultAddress
     ) public {
-        fish = _fish;
+        give = _give;
         startBlock = _startBlock;
 
         devAddress = _devAddress;
@@ -117,12 +117,12 @@ contract MasterChef is Ownable, ReentrancyGuard {
             lpToken: _lpToken,
             allocPoint: _allocPoint,
             lastRewardBlock: lastRewardBlock,
-            accFishPerShare: 0,
+            accGivePerShare: 0,
             depositFeeBP: _depositFeeBP
         }));
     }
 
-    // Update the given pool's FISH allocation point and deposit fee. Can only be called by the owner.
+    // Update the given pool's GIVE allocation point and deposit fee. Can only be called by the owner.
     function set(uint256 _pid, uint256 _allocPoint, uint16 _depositFeeBP) external onlyOwner {
         require(_depositFeeBP <= 10000, "set: invalid deposit fee basis points");
         totalAllocPoint = totalAllocPoint.sub(poolInfo[_pid].allocPoint).add(_allocPoint);
@@ -135,18 +135,18 @@ contract MasterChef is Ownable, ReentrancyGuard {
         return _to.sub(_from);
     }
 
-    // View function to see pending FISHes on frontend.
-    function pendingFish(uint256 _pid, address _user) external view returns (uint256) {
+    // View function to see pending GIVEes on frontend.
+    function pendingGive(uint256 _pid, address _user) external view returns (uint256) {
         PoolInfo storage pool = poolInfo[_pid];
         UserInfo storage user = userInfo[_pid][_user];
-        uint256 accFishPerShare = pool.accFishPerShare;
+        uint256 accGivePerShare = pool.accGivePerShare;
         uint256 lpSupply = pool.lpToken.balanceOf(address(this));
         if (block.number > pool.lastRewardBlock && lpSupply != 0) {
             uint256 multiplier = getMultiplier(pool.lastRewardBlock, block.number);
-            uint256 fishReward = multiplier.mul(fishPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
-            accFishPerShare = accFishPerShare.add(fishReward.mul(1e18).div(lpSupply));
+            uint256 giveReward = multiplier.mul(givePerBlock).mul(pool.allocPoint).div(totalAllocPoint);
+            accGivePerShare = accGivePerShare.add(giveReward.mul(1e18).div(lpSupply));
         }
-        return user.amount.mul(accFishPerShare).div(1e18).sub(user.rewardDebt);
+        return user.amount.mul(accGivePerShare).div(1e18).sub(user.rewardDebt);
     }
 
     // Update reward variables for all pools. Be careful of gas spending!
@@ -169,14 +169,14 @@ contract MasterChef is Ownable, ReentrancyGuard {
             return;
         }
         uint256 multiplier = getMultiplier(pool.lastRewardBlock, block.number);
-        uint256 fishReward = multiplier.mul(fishPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
-        fish.mint(devAddress, fishReward.div(10));
-        fish.mint(address(this), fishReward);
-        pool.accFishPerShare = pool.accFishPerShare.add(fishReward.mul(1e18).div(lpSupply));
+        uint256 giveReward = multiplier.mul(givePerBlock).mul(pool.allocPoint).div(totalAllocPoint);
+        give.mint(devAddress, giveReward.div(10));
+        give.mint(address(this), giveReward);
+        pool.accGivePerShare = pool.accGivePerShare.add(giveReward.mul(1e18).div(lpSupply));
         pool.lastRewardBlock = block.number;
     }
 
-    // Deposit LP tokens to MasterChef for FISH allocation.
+    // Deposit LP tokens to MasterChef for GIVE allocation.
     function deposit(uint256 _pid, uint256 _amount, address _referrer) public nonReentrant {
         PoolInfo storage pool = poolInfo[_pid];
         UserInfo storage user = userInfo[_pid][msg.sender];
@@ -185,9 +185,9 @@ contract MasterChef is Ownable, ReentrancyGuard {
             referral.recordReferral(msg.sender, _referrer);
         }
         if (user.amount > 0) {
-            uint256 pending = user.amount.mul(pool.accFishPerShare).div(1e18).sub(user.rewardDebt);
+            uint256 pending = user.amount.mul(pool.accGivePerShare).div(1e18).sub(user.rewardDebt);
             if (pending > 0) {
-                safeFishTransfer(msg.sender, pending);
+                safeGiveTransfer(msg.sender, pending);
                 payReferralCommission(msg.sender, pending);
             }
         }
@@ -202,7 +202,7 @@ contract MasterChef is Ownable, ReentrancyGuard {
                 user.amount = user.amount.add(_amount);
             }
         }
-        user.rewardDebt = user.amount.mul(pool.accFishPerShare).div(1e18);
+        user.rewardDebt = user.amount.mul(pool.accGivePerShare).div(1e18);
         emit Deposit(msg.sender, _pid, _amount);
     }
 
@@ -212,16 +212,16 @@ contract MasterChef is Ownable, ReentrancyGuard {
         UserInfo storage user = userInfo[_pid][msg.sender];
         require(user.amount >= _amount, "withdraw: not good");
         updatePool(_pid);
-        uint256 pending = user.amount.mul(pool.accFishPerShare).div(1e18).sub(user.rewardDebt);
+        uint256 pending = user.amount.mul(pool.accGivePerShare).div(1e18).sub(user.rewardDebt);
         if (pending > 0) {
-            safeFishTransfer(msg.sender, pending);
+            safeGiveTransfer(msg.sender, pending);
             payReferralCommission(msg.sender, pending);
         }
         if (_amount > 0) {
             user.amount = user.amount.sub(_amount);
             pool.lpToken.safeTransfer(address(msg.sender), _amount);
         }
-        user.rewardDebt = user.amount.mul(pool.accFishPerShare).div(1e18);
+        user.rewardDebt = user.amount.mul(pool.accGivePerShare).div(1e18);
         emit Withdraw(msg.sender, _pid, _amount);
     }
 
@@ -236,16 +236,16 @@ contract MasterChef is Ownable, ReentrancyGuard {
         emit EmergencyWithdraw(msg.sender, _pid, amount);
     }
 
-    // Safe fish transfer function, just in case if rounding error causes pool to not have enough FISH.
-    function safeFishTransfer(address _to, uint256 _amount) internal {
-        uint256 fishBal = fish.balanceOf(address(this));
+    // Safe give transfer function, just in case if rounding error causes pool to not have enough GIVE.
+    function safeGiveTransfer(address _to, uint256 _amount) internal {
+        uint256 giveBal = give.balanceOf(address(this));
         bool transferSuccess = false;
-        if (_amount > fishBal) {
-            transferSuccess = fish.transfer(_to, fishBal);
+        if (_amount > giveBal) {
+            transferSuccess = give.transfer(_to, giveBal);
         } else {
-            transferSuccess = fish.transfer(_to, _amount);
+            transferSuccess = give.transfer(_to, _amount);
         }
-        require(transferSuccess, "safeFishTransfer: Transfer failed");
+        require(transferSuccess, "safeGiveTransfer: Transfer failed");
     }
 
     // Update dev address by the previous dev.
@@ -264,10 +264,10 @@ contract MasterChef is Ownable, ReentrancyGuard {
         emit SetVaultAddress(msg.sender, _vaultAddress);
     }
     
-    function updateEmissionRate(uint256 _fishPerBlock) external onlyOwner {
+    function updateEmissionRate(uint256 _givePerBlock) external onlyOwner {
         massUpdatePools();
-        fishPerBlock = _fishPerBlock;
-        emit UpdateEmissionRate(msg.sender, _fishPerBlock);
+        givePerBlock = _givePerBlock;
+        emit UpdateEmissionRate(msg.sender, _givePerBlock);
     }
 
     // Update the referral contract address by the owner
@@ -289,7 +289,7 @@ contract MasterChef is Ownable, ReentrancyGuard {
             uint256 commissionAmount = _pending.mul(referralCommissionRate).div(10000);
 
             if (referrer != address(0) && commissionAmount > 0) {
-                fish.mint(referrer, commissionAmount);
+                give.mint(referrer, commissionAmount);
                 emit ReferralCommissionPaid(_user, referrer, commissionAmount);
             }
         }
